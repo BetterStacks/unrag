8e627e4879533b194f929f8c130c2fa1d8cbfb66
Subhajit Kundu <subha60kundu@gmail.com>
2025-12-18 09:23:03 +0530

    feat: improve documentation presentation

--- apps/web/mdx-components.tsx
+++ apps/web/mdx-components.tsx
@@ -1,9 +1,24 @@
 import defaultMdxComponents from 'fumadocs-ui/mdx';
+import { Step, Steps } from 'fumadocs-ui/components/steps';
+import { Tab, Tabs } from 'fumadocs-ui/components/tabs';
+import { Accordion, Accordions } from 'fumadocs-ui/components/accordion';
+import { File, Folder, Files } from 'fumadocs-ui/components/files';
+import { TypeTable } from 'fumadocs-ui/components/type-table';
 import type { MDXComponents } from 'mdx/types';
 
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
+    Step,
+    Steps,
+    Tab,
+    Tabs,
+    Accordion,
+    Accordions,
+    File,
+    Folder,
+    Files,
+    TypeTable,
     ...components,
   };
 }
